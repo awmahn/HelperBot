7475e7604c430babe69b3177a9ef9537350ae520
awmahn <Celeron1@>
2017-01-31 09:27:27 -0500

    End of 1/31 class commit.

--- app/src/main/java/com/example/awmahn/helperbot/MainActivity.java
+++ app/src/main/java/com/example/awmahn/helperbot/MainActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    // This is a test comment -awmahn
+    // This is a test comment
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
