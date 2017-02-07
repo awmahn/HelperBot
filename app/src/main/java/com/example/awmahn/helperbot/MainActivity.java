ea4c6ce081ee257fd934f91029308b2486447e62
Robert Curtis <robert@curtiscluster.com>
2017-02-07 14:40:34 -0500

    search button for fish implemented

--- app/src/main/java/com/example/awmahn/helperbot/MainActivity.java
+++ app/src/main/java/com/example/awmahn/helperbot/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.awmahn.helperbot;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 //test text for push check try 5
@@ -12,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void GoogleSearch(View view) {
+        Uri uri = Uri.parse("http://www.google.com/#q=fish");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
 }
